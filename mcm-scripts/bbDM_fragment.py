1cdd474cd90b850502083cf9b0dffea5fd100b33
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2026-04-30 11:20:21 +0800

    Refactor generator setup with grid points

--- mcm-scripts/bbDM_fragment.py
+++ mcm-scripts/bbDM_fragment.py
@@ -1,36 +1,168 @@
 import FWCore.ParameterSet.Config as cms
 
-externalLHEProducer = cms.EDProducer('ExternalLHEProducer',
-    args = cms.vstring('/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_50_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz'),
-    nEvents = cms.untracked.uint32(5000),
-    numberOfParameters = cms.uint32(1),
-    outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
-    generateConcurrently = cms.untracked.bool(False)
-)
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP3Settings_cfi import *
-from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
 from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
-generator = cms.EDFilter("Pythia8HadronizerFilter",
-    PythiaParameters = cms.PSet(
-        pythia8CommonSettingsBlock,
-        pythia8CP3SettingsBlock,
-        pythia8aMCatNLOSettingsBlock,
-        pythia8PSweightsSettingsBlock,
-        parameterSets = cms.vstring(
-            'pythia8CommonSettings',
-            'pythia8CP3Settings',
-            'pythia8PSweightsSettings'
-        )
-    ),
-    comEnergy = cms.double(13600),
-    maxEventsToPrint = cms.untracked.int32(1),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    pythiaPylistVerbosity = cms.untracked.int32(1),
+
+generator = cms.EDFilter(
+    "Pythia8GeneratorFilter",
+    maxEventsToPrint=cms.untracked.int32(1),
+    pythiaPylistVerbosity=cms.untracked.int32(1),
+    filterEfficiency=cms.untracked.double(1.0),
+    pythiaHepMCVerbosity=cms.untracked.bool(False),
+    comEnergy=cms.double(13600.0),
+    RandomizedParameters=cms.VPSet(),
 )
 
+grid_points = [
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_1000_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_1000_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_100_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_100_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_10_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_10_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_1150_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_1150_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_1300_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_1300_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_1450_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_1450_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_150_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_150_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_400_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_400_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_450_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_450_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_500_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_500_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_50_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_50_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_600_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_600_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_700_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_700_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_1500_MH4_850_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_1500_MH4_850_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_100_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_100_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_10_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_10_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_150_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_150_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_200_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_200_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_250_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_250_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_300_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_300_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_350_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_350_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_400_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_400_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_450_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_450_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_500_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_500_Mchi_1",
+        "weight": 0.04,
+    },
+    {
+        "gridpack_path": "/eos/cms/store/group/phys_susy/sus-23-008/run3_2HDMa_typeII_bbdm_gridpacks/bbDM_2HDMa_MH3_600_MH4_50_Mchi_1_el8_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz",
+        "name": "MH3_600_MH4_50_Mchi_1",
+        "weight": 0.04,
+    },
+]
+
+for grid_point in grid_points:
+    basePythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP3SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        parameterSets=cms.vstring(
+            "pythia8CommonSettings",
+            "pythia8CP3Settings",
+            "pythia8PSweightsSettings",
+        ),
+    )
+
+    generator.RandomizedParameters.append(
+        cms.PSet(
+            ConfigWeight=cms.double(grid_point["weight"]),
+            ConfigDescription=cms.string(grid_point["name"]),
+            PythiaParameters=basePythiaParameters,
+            GridpackPath=cms.string(grid_point["gridpack_path"]),
+        )
+    )
 
 ProductionFilterSequence = cms.Sequence(generator)
